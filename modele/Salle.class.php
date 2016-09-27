35b88d497ed2b69926e90967881e48d75ccd4f8e
delasalle-sio-leguen-k <delasalle.sio.leguen.k@gmail.com>
2016-09-27 17:31:58 +0200

    Salle.class

--- modele/Salle.class.php
+++ modele/Salle.class.php
@@ -2,14 +2,54 @@
 // Projet Réservations M2L - version web mobile
 // fichier : 
 // Rôle : 
-// Création : 
+// Création : 27/09/2016
 // Mise à jour : 
 
 
 
+class Salle {
+	// Eléments de notre panier
+	private $id;
+	private $room_name;
+	private $capacity;
+	private $areaName;
+	
+	public function Salle($unId, $unRoomName, $unCapacity, $unAreaName) {
+		$this->id = $unId;
+		$this->room_name = $unRoomName;
+		$this->capacity = $unCapacity;
+		$this->areaName = $unAreaName;		
+	}
 
+	// Ajout de $num articles de type $artnr au panier
 
+	public function getId()	{return $this->id;}
+	public function setId($unId) {$this->Id = $unId;}
+	
+	public function getRoom_name()	{return $this->room_name;}
+	public function setRoom_name($unRoomName) {$this->room_name = $unRoomName;}
+	
+	public function getCapacity()	{return $this->capacity;}
+	public function setCapacity($unCapacity) {$this->capacity = $unCapacity;}
+	
+	public function getAreaName()	{return $this->areaName;}
+	public function setAreaName($unAreaName) {$this->areaName = $unAreaName;}
+	
+	
+	public function toString() {
+	
+		$msg = 'Id : ' . $this->getId() . '<br>';
+		$msg = 'RoomName : ' . $this->getRoom_name() . '<br>';
+		$msg = 'Capacity : ' . $this->getCapacity() . '<br>';
+		$msg = 'AreaName : ' . $this->getAreaName() . '<br>';
+		$msg = '<br>';
 
+		Return $msg;
+	}
+	
+	
+
+}
 
 
 
